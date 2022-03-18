abdb0a58d94c40faa5ef16487e6b799331336165
twpkevin06222 <tengwaipokevin@gmail.com>
2022-03-18 17:18:47 +0800

    second commit

---
+++ model.py
@@ -0,0 +1,190 @@
+import torch
+from torch import nn
+from torch import Tensor
+from torchsummary import summary
+import torch.nn.functional as F
+# Get cpu or gpu device for training
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+class SeparableConv2d(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int,
+                 stride: tuple, kernel_size: tuple = (3, 3),
+                 padding: str or int = 'same') -> None:
+        super(SeparableConv2d, self).__init__()
+        self.depthwise = nn.Conv2d(in_channels, in_channels, kernel_size=kernel_size,
+                                   stride=stride, padding=padding)
+        self.pointwise = nn.Conv2d(in_channels, out_channels, kernel_size=(1, 1))
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = self.depthwise(x)
+        x = self.pointwise(x)
+
+        return x
+
+
+class ConvBlock(nn.Module):
+    """
+    Pre-activation convblock:
+    batch norm -> activation -> weights
+    """
+    def __init__(self, in_channels: int, out_channels: int,
+                 stride: tuple = (1, 1), dropout: float = 0.2) -> None:
+        super(ConvBlock, self).__init__()
+        self.bn = nn.BatchNorm2d(in_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv = SeparableConv2d(in_channels, out_channels,
+                                    stride=stride)
+        self.drop = nn.Dropout2d(dropout)
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = self.bn(x)
+        x = self.relu(x)
+        x = self.conv(x)
+        x = self.drop(x)
+
+        return x
+
+
+class UpBlock(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int,
+                 kernel_size: tuple = (3, 3)) -> None:
+        super(UpBlock, self).__init__()
+        self.up = nn.Upsample(scale_factor=2, mode='nearest')
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size)
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = self.up(x)
+        x = self.conv(x)
+
+        return x
+
+
+class UpConvBlock(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int,
+                 kernel_size: tuple = (3, 3)) -> None:
+        super(UpConvBlock, self).__init__()
+        self.up = UpBlock(in_channels, out_channels, kernel_size)
+        self.convblock1 = ConvBlock(out_channels*2, out_channels)
+        self.convblock2 = ConvBlock(out_channels, out_channels)
+
+    def forward(self, x: Tensor, enc_feature: Tensor) -> Tensor:
+        x = self.up(x)
+        x = torch.cat([x, enc_feature], dim=1)
+        x = self.convblock1(x)
+        x = self.convblock2(x)
+
+        return x
+
+
+class ResBlock(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int,
+                 downsampling: bool = True) -> None:
+        super(ResBlock, self).__init__()
+        self.downsampling = downsampling
+        if self.downsampling:
+            self.down_conv = SeparableConv2d(in_channels, out_channels,
+                                             stride=(2, 2), padding='valid')
+            self.convblock1 = ConvBlock(out_channels, out_channels)
+        else:
+            self.convblock1 = ConvBlock(in_channels, out_channels)
+        self.convblock2 = ConvBlock(out_channels, out_channels)
+
+    def forward(self, x: Tensor) -> Tensor:
+        if self.downsampling:
+            # padding to maintain the power of 2 dimensionality
+            x = F.pad(x, (1, 1, 1, 1), 'constant', 0)
+            x = self.down_conv(x)
+        identity = x
+        x = self.convblock1(x)
+        x = self.convblock2(x)
+        x += identity
+
+        return x
+
+
+class Encoder(nn.Module):
+    """
+    Resnet inspired encoder
+    """
+    def __init__(self, in_channels: int, init_filter: int,
+                 depth: int) -> None:
+        super(Encoder, self).__init__()
+        self.init_filter = init_filter
+        self.in_channels = in_channels
+        self.depth = depth
+        self.conv = SeparableConv2d(in_channels, init_filter, stride=(1,1))
+        self.resblock0 = ResBlock(init_filter, init_filter, downsampling=False)
+        self.resblock = nn.ModuleList([ResBlock(self.init_filter*(2**i), self.init_filter*(2**(i+1)))
+                                       for i in range(depth)])
+        self.bn = nn.BatchNorm2d(init_filter*(2**(depth)))
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x: Tensor) -> list:
+        block_outputs = []
+        x = self.conv(x)
+        x = self.resblock0(x)
+        block_outputs.append(x)
+        for i, block in enumerate(self.resblock):
+            x = block(x)
+            if i is self.depth-1:
+                x = self.bn(x)
+                x = self.relu(x)
+            block_outputs.append(x)
+
+        return block_outputs
+
+
+class Decoder(nn.Module):
+    def __init__(self, init_filter: int,
+                 depth: int) -> None:
+        super(Decoder, self).__init__()
+        self.init_filter = init_filter
+        self.depth = depth
+        self.conv = nn.Conv2d(init_filter, 1, kernel_size=(3, 3),
+                              stride=(1, 1), padding='same')
+        self.upblock = nn.ModuleList([UpConvBlock(self.init_filter*(2**i), self.init_filter*(2**(i-1)))
+                                     for i in range(depth, 0, -1)])
+        self.bn1 = nn.BatchNorm2d(init_filter)
+        self.relu = nn.ReLU(inplace=True)
+        self.bn2 = nn.BatchNorm2d(1)
+
+    def forward(self, enc_feature: list) -> Tensor:
+        x = self.upblock[0](enc_feature[3], enc_feature[2])
+        # x = self.upblock[1](x, enc_feature[1])
+        # x = self.upblock[2](x, enc_feature[0])
+        # x = self.bn1(x)
+        # x = self.relu(x)
+        # x = self.conv(x)
+        # x = self.bn2(x)
+
+        return x
+
+
+class UNet(nn.Module):
+    def __init__(self, in_channels: int,
+                 init_filter: int, depth: int):
+        super(UNet, self).__init__()
+        self.enc = Encoder(in_channels, init_filter, depth)
+        self.dec = Decoder(init_filter, depth)
+
+    def forward(self, x):
+        enc_feature = self.enc(x)
+        output = self.dec(enc_feature)
+
+        return output
+
+
+if __name__ == '__main__':
+    tensor = torch.ones(8, 3, 512, 512).to(device)
+    model = Encoder(3, 64, 3).to(device)
+    dec = Decoder(64, 3).to(device)
+    output = model(tensor)
+    for i in output:
+        print(i.shape)
+    # dec_output = dec(output)
+    # print(dec_output.shape)
+    # model = UNet(3, 64, 3).to(device)
+    # summary(model, (3, 512, 512))
+
+
---
+++ train.py
@@ -0,0 +1,181 @@
+import warnings
+import numpy as np
+import os
+import utils
+import dataloader
+import pandas as pd
+from batchgenerators.dataloading.multi_threaded_augmenter import MultiThreadedAugmenter
+import wandb
+from tqdm import tqdm
+import torch
+from torch import nn
+import segmentation_models_pytorch as smp
+from torchsummary import summary
+import math
+import time
+
+warnings.filterwarnings('ignore')
+wandb.init(project='Gland_Seg', entity='glaseg', config='config/config.yaml')
+config = wandb.config
+tabular_data = pd.read_csv(config.csv)
+ds_dict = dataloader.get_split_fold(tabular_data)
+patch_size = eval(config.patch_size)
+batch_size = config.batch_size
+epochs = config.epochs
+tr_transforms = dataloader.get_train_transform(patch_size, prob=config.aug_prob)
+train_dl = dataloader.DataLoader(data=ds_dict['train_ds'], batch_size=batch_size, patch_size=patch_size,
+                                 num_threads_in_multithreaded=4, seed_for_shuffle=5243,
+                                 return_incomplete=False, shuffle=True, infinite=True)
+train_gen = MultiThreadedAugmenter(train_dl, tr_transforms, num_processes=4,
+                                   num_cached_per_queue=2,
+                                   seeds=None, pin_memory=False)
+val_dl = dataloader.DataLoader(data=ds_dict['testA_ds'], batch_size=batch_size, patch_size=patch_size,
+                               num_threads_in_multithreaded=1, seed_for_shuffle=5243,
+                               return_incomplete=False, shuffle=True, infinite=True)
+
+# Get cpu or gpu device for training
+device = "cuda" if torch.cuda.is_available() else "cpu"
+# define model
+model = smp.Unet(encoder_name=config.encoder_model, decoder_use_batchnorm=True,
+                 in_channels=3, classes=config.n_class).to(device)
+summary(model, (3, 512, 512))
+optimizer = eval(config.optimizer)(model.parameters(), lr=float(config.learning_rate))
+# xent = nn.BCELoss()
+dice_loss = smp.losses.DiceLoss(smp.losses.BINARY_MODE, from_logits=True)
+# label smoothing
+xent = smp.losses.SoftBCEWithLogitsLoss(smooth_factor=0.1)
+
+
+def custom_loss(pred, target):
+    xent_l = xent(pred, target)
+    dice_l = dice_loss(pred, target)
+    loss = xent_l + dice_l
+    return loss, xent_l, dice_l
+
+
+def train(model, optimizer):
+    # total number of training batches
+    num_batches = math.ceil(len(ds_dict['train_ds']['img_npy'])/batch_size)
+    model.train()
+    batch_xent_l = []
+    batch_dice_l = []
+    batch_loss = []
+    print("Training...")
+    for i in tqdm(range(num_batches)):
+        train_batch = next(train_gen)
+        imgs = train_batch['data']
+        segs = train_batch['seg']
+        # normalization
+        imgs = utils.min_max_norm(imgs)
+        # binarisation
+        segs = np.where(segs > 0., 1.0, 0.).astype('float32')
+        segs = np.expand_dims(segs[:, 0, :, :], 1)
+        imgs, segs = torch.from_numpy(imgs).to(device), torch.from_numpy(segs).to(device)
+        # Compute loss
+        pred = model(imgs)
+        loss, xent_l, dice_l = custom_loss(pred, segs)
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        # batch losses
+        batch_xent_l.append(xent_l)
+        batch_dice_l.append(dice_l)
+        batch_loss.append(loss)
+    # apply sigmoid to masking
+    segs = nn.Sigmoid()(segs)
+    # taking the average along the batch
+    loss = torch.mean(torch.as_tensor(batch_loss)).item()
+    avg_xent_l = torch.mean(torch.as_tensor(batch_xent_l)).item()
+    avg_dice_l = torch.mean(torch.as_tensor(batch_dice_l)).item()
+
+    return {'loss': loss, 'xent_l': avg_xent_l, 'dice_l': avg_dice_l,
+            'imgs': imgs.cpu().detach().numpy(),
+            'segs': segs.cpu().detach().numpy(),
+            'pred': pred.cpu().detach().numpy()}
+
+
+def test(model):
+    num_batches = math.ceil(len(ds_dict['testA_ds']['img_npy']) / batch_size)
+    model.eval()
+    test_loss, correct = 0, 0
+    # no need back prop for testing set
+    batch_xent_l = []
+    batch_dice_l = []
+    batch_loss = []
+    print("Testing...")
+    with torch.no_grad():
+        for i in tqdm(range(num_batches)):
+            val_batch = next(val_dl)
+            imgs = val_batch['data']
+            segs = val_batch['seg']
+            # normalization
+            imgs = utils.min_max_norm(imgs)
+            # binarisation
+            segs = np.where(segs > 0., 1.0, 0.).astype('float32')
+            segs = np.expand_dims(segs[:, 0, :, :], 1)
+            imgs, segs = torch.from_numpy(imgs).to(device), torch.from_numpy(segs).to(device)
+            # Compute loss
+            pred = model(imgs)
+            loss, xent_l, dice_l = custom_loss(pred, segs)
+            # batch losses
+            batch_xent_l.append(xent_l)
+            batch_dice_l.append(dice_l)
+            batch_loss.append(loss)
+        # apply sigmoid to masking
+        segs = nn.Sigmoid()(segs)
+        # taking the average along the batch
+        loss = torch.mean(torch.as_tensor(batch_loss)).item()
+        avg_xent_l = torch.mean(torch.as_tensor(batch_xent_l)).item()
+        avg_dice_l = torch.mean(torch.as_tensor(batch_dice_l)).item()
+    return {'loss': loss, 'xent_l': avg_xent_l, 'dice_l': avg_dice_l,
+            'imgs': imgs.cpu().detach().numpy(),
+            'segs': segs.cpu().detach().numpy(),
+            'pred': pred.cpu().detach().numpy()}
+
+start = time.time()
+for e in range(1, epochs+2):
+    current_total_loss = 1000
+    current_dice_score = 0
+    print("Epcohs:", e)
+    train_output = train(model, optimizer)
+    test_output = test(model)
+    print("Training Outputs: ")
+    print("Total loss: {:.2f}, BCE: {:.2f}, Dice Score: {:2f}".format(train_output['loss'], train_output['xent_l'], 1 - train_output['dice_l']))
+    print("-"*100)
+    print("Validation Outputs: ")
+    print("Total loss: {:.2f}, BCE: {:.2f}, Dice Score: {:2f}".format(test_output['loss'], test_output['xent_l'], 1-test_output['dice_l']))
+    # logging
+    wandb.log({"Train_total_loss": train_output['loss'], "Val_total_loss": test_output['loss']}, step=e)
+    wandb.log({"Train_BCE_loss": train_output['xent_l'], "Val_BCE_loss": test_output['xent_l']}, step=e)
+    wandb.log({"Train_dice_score": 1-train_output['dice_l'], "Val_dice_score": 1-test_output['dice_l']}, step=e)
+
+    if e%10==0:
+        # threshold sigmoid output with 0.5
+        pred_thr = np.where(test_output['pred']>0.5, 1.0, 0.0)
+        # sample a dataset from the batch for visualization purpose
+        imgs = [test_output['imgs'][0, 0, :, :], test_output['segs'][0, 0, :, :], pred_thr[0, 0, :, :]]
+        captions = ['Gland Image', 'Masking', 'Prediction']
+        fig = utils.plot_comparison(imgs, captions, plot=False, n_col=len(imgs),
+                              figsize=(12, 12), cmap='gray')
+        wandb.log({"Validation Dataset Output Sample": wandb.Image(fig)}, step=e)
+
+    # save model
+    weights_dir = '/home/kevinteng/Desktop/weights/'
+    if not os.path.exists(weights_dir):
+        os.makedirs(weights_dir)
+    base_path = os.path.split(weights_dir)[0]
+    if test_output['loss'] < current_total_loss:
+        current_total_loss = test_output['loss']
+        torch.save(model.state_dict(), weights_dir+'best_loss_{}.pth'.format(e))
+        wandb.save(os.path.join(weights_dir, 'best_loss_{}.pth'.format(e)), base_path=base_path)
+    if (1-test_output['dice_l']) > current_dice_score:
+        current_dice_score = 1-test_output['dice_l']
+        torch.save(model.state_dict(), weights_dir+'best_dice_{}.pth'.format(e))
+        wandb.save(os.path.join(weights_dir, 'best_dice_{}.pth'.format(e)), base_path=base_path)
+    print()
+
+print("Model training runtime: {} mins".format((time.time() - start)/60.0))
+
+
+
